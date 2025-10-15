706cb32204556d4bf53496394556bcd9fccc7e0a
sunil <sunilragavmech.1999@gmail.com>
2025-10-15 22:42:55 +0530

    kiya-bday

---
+++ src/data.js
@@ -0,0 +1,92 @@
+// Your Birthday Date (Format: yyyy-mm-ddTHH:MM:SS)
+export const BIRTHDAY_DATE = "2025-10-16T00:00:00";
+
+// Add recipient's name here.
+export const NAME = "Kiya"
+
+
+// Special Messages Screen
+// You can edit each card from here.
+// - "title" is optional, if you don’t want it, just remove it.
+// - "showIcon" is true to show icon, false to hide it.
+// - You can change the "color" and "message" as you like.
+// export const messages = [
+//     {
+//         id: 1,
+//         color: "from-pink-500 to-rose-500",
+//         title: " ",
+//         message: "I am happy for yours friendship💖",
+//         showIcon: true,
+//     },
+    // {
+    //     id: 2,
+    //     color: "from-yellow-500 to-orange-500",
+    //     title: "Big Dreams",
+    //     message: "Ipa onakum enakum iruka big dream namma marriage tha kandipaaa next year paniralam babyyyy✨",
+    //     showIcon: true,
+    // },
+    // {
+    //     id: 3,
+    //     color: "from-purple-500 to-indigo-500",
+    //     title: "New Adventures",
+    //     message: "Every step with you feels like a dream in motion,Your laugh is my favorite destination✨Nam kadhal oru travel da,Full of smiles,surprises,and sweet emotions💖",
+    //     showIcon: true,
+    // },
+    // {
+    //     id: 4,
+    //     color: "from-green-500 to-teal-500",
+    //     message: "Un presence dhaan da enaku peace ah iruku✨Enna paninaalum, nee dhaan en first thought & last smile❤️Un kindness, un vibe — romba special thimasha🌸Nee dhaan en life la best part 💫",
+    //     title: "You're Amazing",
+    //     showIcon: true,
+    // },
+// ];
+
+
+// Photo Gallery Screen
+// Heading text for the Photo Gallery section (displayed at the top)
+export const photoScreenHeading = ""
+
+// Subheading text for the Photo Gallery section (optional - you can remove or leave it blank if not needed)
+export const photoScreenSubHeading = ""
+
+// List of photo objects to display in the gallery
+// - Add more photos by copying the same line and changing the id and src
+// - Make sure each id is unique and the src points to a valid image path
+// Make sure all image files are placed in the `/public/images/` folder
+export const photos = [
+    { id: 1, src: "/images/1.jpeg" },
+    { id: 2, src: "/images/2.gif" },
+    { id: 3, src: "/images/3.gif" },
+    { id: 4, src: "/images/4.jpeg" },
+    { id: 5, src: "/images/5.jpeg" },
+    { id: 6, src: "/images/6.jpeg" },
+    { id: 7, src: "/images/7.gif" },
+    { id: 8, src: "/images/8.gif" },
+    { id: 9, src: "/images/9.jpeg" },
+    { id: 10, src: "/images/10.jpeg" },
+    { id: 11, src: "/images/11.gif" },
+    
+    
+    
+
+]
+
+
+// Letter Screen
+// Main heading for the letter screen
+export const letterScreenHeading = "Open it"
+
+// Optional subheading for the letter screen
+// If you don't want any subheading, just leave it blank like this: ""
+// export const letterScreenSubHeading = "Just for you, on your special day 💌"
+
+// Actual letter content to display on the screen
+export const letterText = ` 
+Nee en life la vandhu enna neriya maaththirukka, ennoda heart la oru special place onakum epaum irukum....I WILL ALWAYS THERE FOR YOU💜🖤
+once again, Happy Birthday PANDA🐼
+
+`
+
+// Background Music file path
+// Place your .mp3 file inside the "public/audio" folder and give the path like below.
+export const backgroundMusic = "/audio/bg.mp3"
